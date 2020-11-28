8e63b4d5c0710b060e5d5a9e10dd6294967b7a9e
matheusferretti <matheusferretti96@gmail.com>
2020-11-28 13:44:40 -0500

    updating

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <h1>Lets build a HULU CLONE matheus only</h1>
+
+      <Header />
+
+      {/* Header */}
+      {/* Nav */}
+      {/* Results */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Header.css";
+
+function Header() {
+    return (
+        //BEM naming class
+        <div className="header">
+            <h1>I am a header</h1>
+        </div>
+    )
+}
+
+export default Header
