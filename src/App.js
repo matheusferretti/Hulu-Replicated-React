4168aaeed57bc8f6595bba55cc479264686b5281
matheusferretti <matheusferretti96@gmail.com>
2020-11-28 14:00:00 -0500

    updating

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ import Header from './Header';
 function App() {
   return (
     <div className="App">
-      <h1>Lets build a HULU CLONE matheus only</h1>
-
       <Header />
 
-      {/* Header */}
       {/* Nav */}
+      
       {/* Results */}
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,10 @@ function Header() {
     return (
         //BEM naming class
         <div className="header">
-            <h1>I am a header</h1>
+            <div className="header__icons">
+                {/* All icons here */}
+            </div>
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.suplari.com%2Fwp-content%2Fuploads%2F2019%2F09%2Fhulu-logo-white.png&f=1&nofb=1" alt=""></img>
         </div>
     )
 }
