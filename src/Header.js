4262e893d3ee9e8e6b6f583e2aaf6f40ce8c4c86
matheusferretti <matheusferretti96@gmail.com>
2020-11-28 15:23:23 -0500

    updating

--- src/Header.js
+++ src/Header.js
@@ -12,12 +12,24 @@ function Header() {
         //BEM naming class
         <div className="header">
             <div className="header__icons">
-                <HomeIcon />
-                <FlashOnIcon />
-                <LiveTvIcon />
-                <VideoLibraryIcon />
-                <SearchIcon />
-                <PersonOutlineIcon />
+                <div className="header__icon">
+                    <HomeIcon />
+                </div>
+                <div className="header__icon">
+                    <FlashOnIcon />
+                </div>
+                <div className="header__icon">
+                    <LiveTvIcon />
+                </div>
+                <div className="header__icon">
+                    <VideoLibraryIcon />
+                </div>
+                <div className="header__icon">
+                    <SearchIcon />
+                </div>
+                <div className="header__icon">
+                    <PersonOutlineIcon />
+                </div>
             </div>
             <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.suplari.com%2Fwp-content%2Fuploads%2F2019%2F09%2Fhulu-logo-white.png&f=1&nofb=1" alt=""></img>
         </div>
