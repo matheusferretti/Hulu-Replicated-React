63234064f5e94e05a04922d5b81b6ce9215aa208
matheusferretti <matheusferretti96@gmail.com>
2020-11-30 17:11:08 -0500

    updating

--- src/Results.js
+++ src/Results.js
@@ -7,6 +7,12 @@ function Results() {
 
     const [movies, setMovies] = useState([])
 
+    useEffect(() => {
+        async function fetchData() {
+            
+        }
+    }, [])
+
     return (
         <div className="results">
             {movies.map((movie) => (
