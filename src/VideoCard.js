f078a8ef1bf9903e5ecccd876a069d169abb1ed1
matheusferretti <matheusferretti96@gmail.com>
2020-11-30 16:08:51 -0500

    updating

---
+++ src/VideoCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./VideoCard.css"
+
+function VideoCard({movie}) {
+    return (
+        <div className="videoCard">
+            <img src="" alt="" />
+
+        </div>
+    )
+}
+
+export default VideoCard
