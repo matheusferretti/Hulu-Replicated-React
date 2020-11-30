b47680e4ad6c82fd26554939eb179c53cf632f28
matheusferretti <matheusferretti96@gmail.com>
2020-11-30 15:43:17 -0500

    updating

--- src/Nav.js
+++ src/Nav.js
@@ -4,7 +4,11 @@ import "./Nav.css";
 function Nav() {
     return (
         <div className="nav">
-            
+            <h2>Trending</h2>
+            <h2>Top Rated</h2>
+            <h2>Action</h2>
+            <h2>Comedy</h2>
+            <h2>Comedy</h2>
         </div>
     )
 }
