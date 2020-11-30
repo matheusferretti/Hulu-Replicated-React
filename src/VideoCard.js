a3f43dcf0037a9fce6135e28c8a9023e229e5095
matheusferretti <matheusferretti96@gmail.com>
2020-11-30 16:18:57 -0500

    updating

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -4,8 +4,10 @@ import "./VideoCard.css"
 function VideoCard({movie}) {
     return (
         <div className="videoCard">
-            <img src="" alt="" />
-
+            <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.otakupt.com%2Fwp-content%2Fuploads%2F2014%2F11%2Fe0450rde0c565999e4c77.jpg&f=1&nofb=1" alt="" />
+            <p>This is a film about bebop...</p>
+            <h2>Movie Title</h2>
+            <p>Number of likes...</p>
         </div>
     )
 }
