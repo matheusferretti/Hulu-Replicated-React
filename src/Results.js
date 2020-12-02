f36df74c2445e89bb2f3661867c942f28cc7d7d3
matheusferretti <matheusferretti96@gmail.com>
2020-12-02 15:51:39 -0500

    updating

--- src/Results.js
+++ src/Results.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import "./Results.css"
 import VideoCard from "./VideoCard"
 import axios from "./axios";
+import FlipMove from "react-flip-move";
 
 function Results({ selectedOption}) {
 
@@ -19,9 +20,11 @@ function Results({ selectedOption}) {
 
     return (
         <div className="results">
-            {movies.map((movie) => (
-            <VideoCard key={movie.id} movie={movie} />
-            ))}
+            <FlipMove>
+                {movies.map((movie) => (
+                <VideoCard key={movie.id} movie={movie} />
+                ))}
+            </FlipMove>
         </div>
     );
 }
