90eee12887e8a7cb2dc76bb33f8df18697e2e4fe
matheusferretti <matheusferretti96@gmail.com>
2020-12-02 15:04:47 -0500

    updating

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./VideoCard.css"
 import TextTruncate from "react-text-truncate";
+import {ThumbUpSharp} from "@material-ui/icons";
 
 const base_url = "https://image.tmdb.org/t/p/original/"; 
 
@@ -10,7 +11,10 @@ function VideoCard({movie}) {
             <img src={`${base_url}${movie.backdrop_path || movie.poster_path}`} alt="movie poster" />
             <TextTruncate line={2} element="p" truncateText="..." text={movie.overview}/>
             <h2>{movie.title || movie.original_name}</h2>
-            <p>{movie.vote_count}</p>
+            <p>
+                {movie.media_type && `${movie.media_type}`}
+
+                <ThumbUpSharp />{movie.vote_count}</p>
         </div>
     )
 }
