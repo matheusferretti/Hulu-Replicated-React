b2480e37a88165f50e85facd60fa37e0372b9894
matheusferretti <matheusferretti96@gmail.com>
2020-12-02 14:44:36 -0500

    updating

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./VideoCard.css"
 
+const base_url = "https://image.tmdb.org/t/p/original/"; 
+
 function VideoCard({movie}) {
     return (
         <div className="videoCard">
-            <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.otakupt.com%2Fwp-content%2Fuploads%2F2014%2F11%2Fe0450rde0c565999e4c77.jpg&f=1&nofb=1" alt="" />
+            <img src={`${base_url}${movie.backdrop_path}`} alt="" />
             <p>This is a film about bebop...</p>
             <h2>Movie Title</h2>
             <p>Number of likes...</p>
