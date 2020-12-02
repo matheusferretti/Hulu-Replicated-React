252a25b6778d43a027dd5cf301de6a2448395d65
matheusferretti <matheusferretti96@gmail.com>
2020-12-02 14:49:22 -0500

    updating

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -7,7 +7,7 @@ function VideoCard({movie}) {
     return (
         <div className="videoCard">
             <img src={`${base_url}${movie.backdrop_path || movie.poster_path}`} alt="movie poster" />
-            <p>{movie.release_date || movie.first_air_date}</p>
+            <p>{movie.overview}</p>
             <h2>Movie Title</h2>
             <p>Number of likes...</p>
         </div>
